fcab0f9d982dc14d8b4a80f7e2685d78ce350d92
Hiroki-150 <k21150kk@aitech.ac.jp>
2022-12-08 15:52:56 +0900

    中島150の日記を追加

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.Nakashima150Diary import Nakashima150Diary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
     DiarySample(),
+    Nakashima150Diary(),
 ] 
 
 for d in diaries:
