b22a18d09472199e60869506ad7e55faf216460c
kk-r-on <kazu.2003.onoda@gmail.com>
2022-12-08 15:29:29 +0900

    最初の日記サンプルを追加

---
+++ diaries/DiarySample.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class DiarySample(AbstractDiary):
+
+    def get_date(self):
+        return "2022-12-08"
+
+    def get_summary(self):
+        return "なにもない一日だった"
+
+    def get_author(self):
+        return "Sample"
